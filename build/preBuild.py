fda370cc223560a02800c767401d305ffe574d61
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-04-09 02:52:19 -0300

    Hack build scripts to work for now

--- build/preBuild.py
+++ build/preBuild.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 Import('env')
+
 from preBuildESP8266Certificates import preBuildCertificates
 
 preBuildCertificates(env)
--- build/preBuildESP32.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-Import('env')
-from preBuildESP32Certificates import preBuildCertificates
-
-preBuildCertificates(env)
