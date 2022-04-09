d757cdd2902bfbc3a8e027bcd419d068a50b74d4
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-04-09 05:23:06 -0300

    Set build flags

--- src/noop/log.hpp
+++ src/noop/log.hpp
@@ -2,8 +2,8 @@
 #include "iop/thread.hpp"
 
 namespace driver {
-void logSetup(const iop::LogLevel &level) noexcept {}
-void logPrint(const iop::StaticString msg) noexcept {}
-void logPrint(const std::string_view msg) noexcept {}
+void logSetup(const iop::LogLevel &level) noexcept { (void) level; }
+void logPrint(const iop::StaticString msg) noexcept { (void) msg; }
+void logPrint(const std::string_view msg) noexcept { (void) msg; }
 void logFlush() noexcept {}
 }
