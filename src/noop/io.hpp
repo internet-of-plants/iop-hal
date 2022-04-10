0084efab4bbcb18e2baa4c313352bd89e710855c
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-04-10 02:59:46 -0300

    PinRaw, not Pin

--- src/noop/io.hpp
+++ src/noop/io.hpp
@@ -2,8 +2,8 @@
 
 namespace iop_hal {
 namespace io {
-void GPIO::setMode(const Pin pin, const Mode mode) const noexcept { (void) pin; (void) mode; }
-auto GPIO::digitalRead(const Pin pin) const noexcept -> Data { (void) pin; return Data::HIGH; }
-void GPIO::setInterruptCallback(const Pin pin, const InterruptState state, void (*func)()) const noexcept { (void) pin; (void) state; (void) func; }
+void GPIO::setMode(const PinRaw pin, const Mode mode) const noexcept { (void) pin; (void) mode; }
+auto GPIO::digitalRead(const PinRaw pin) const noexcept -> Data { (void) pin; return Data::HIGH; }
+void GPIO::setInterruptCallback(const PinRaw pin, const InterruptState state, void (*func)()) const noexcept { (void) pin; (void) state; (void) func; }
 } // namespace io
 } // namespace iop_hal
