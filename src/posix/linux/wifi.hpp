7a4e9fd4e7d50f1bd6cf6572a23bafb649a0e2b7
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2023-03-28 00:08:27 -0300

    Remove broken desktop wifi integation

--- src/posix/linux/wifi.hpp
+++ src/posix/linux/wifi.hpp
@@ -37,9 +37,6 @@ namespace iop_hal {
 Wifi::Wifi() noexcept {}
 Wifi::~Wifi() noexcept {}
 
-// TODO FIXME: the functions below are buggy
-// TODO: handle failure
-
 void Wifi::setup() noexcept {
   HTTPClient::setup();
 }
@@ -58,51 +55,25 @@ StationStatus Wifi::status() const noexcept {
 }
 
 bool Wifi::connectToAccessPoint(std::string_view ssid, std::string_view psk) const noexcept {
-  // TODO FIXME: RCE here, this is super dumb, it's just to make everything work and it takes into account ssid and psk are hardcoded
-  // Please do not trust user input in this, please
-  //return 0 == std::system((std::string("nmcli dev wifi connect ") + std::string(ssid) + " password " + std::string(psk)).c_str());
   (void) ssid;
   (void) psk;
   return true;
 }
 
-void Wifi::disconnectFromAccessPoint() const noexcept {
-  //auto code = std::system("nmcli radio wifi off");
-  //code = std::system("nmcli radio wifi on");
-  //(void) code;
-}
+void Wifi::disconnectFromAccessPoint() const noexcept {}
 
 void Wifi::enableOurAccessPoint(std::string_view ssid, std::string_view psk) const noexcept {
-  // TODO FIXME: RCE here, this is super dumb, it's just to make everything work and it takes into account ssid and psk are hardcoded
-  // Please do not trust user input in this, please
-  //auto code = std::system((std::string("nmcli dev wifi hotspot ssid ") + std::string(ssid.substr(0, 32)) + std::string(" password ") + std::string(psk.substr(0, 64))).c_str());
-  //(void) code;
   (void) ssid;
   (void) psk;
 }
 
-auto Wifi::disableOurAccessPoint() const noexcept -> void {
-  //auto code = std::system("nmcli radio wifi off");
-  //code = std::system("nmcli radio wifi on");
-  //(void) code;
-}
+auto Wifi::disableOurAccessPoint() const noexcept -> void {}
 
 void Wifi::setMode(WiFiMode mode) const noexcept {
-  //int code;
-  switch (mode) {
-    case WiFiMode::OFF:
-      //code = std::system("nmcli radio wifi off");
-      break;
-    case WiFiMode::STATION:
-    case WiFiMode::ACCESS_POINT:
-    case WiFiMode::ACCESS_POINT_AND_STATION:
-      //code = std::system("nmcli radio wifi on");
-      break;
-  }
-  //(void) code;
+  (void) mode;
 }
 
-// NOOP
+// FIXME TODO: Lies
 std::pair<iop::NetworkName, iop::NetworkPassword> Wifi::credentials() const noexcept {
   IOP_TRACE()
   iop::NetworkName ssid;
